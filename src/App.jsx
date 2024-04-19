29962df665644ee2bd82e87dfc0937ff22b65400
mrlane51 <mrlane51@ymail.com>
2024-04-19 12:34:22 -0500

    added components and editing main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
-  
-  return (
-    <h1 className="text-3xl font-bold underline">
-      LoTechTX under Construction ...
-    </h1>
-  )
-}
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
-export default App
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        LoTechTX under Construction ...
+      </h1>
+      ;
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Click
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
